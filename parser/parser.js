b708698218dc7d5069c89c3bf0c64af123dfd727
nagnagi <nag.nagipon@gmail.com>
2024-12-17 13:51:44 +0900

    括弧の置き換えできた！！！

--- parser/parser.js
+++ parser/parser.js
@@ -27,12 +27,57 @@ const definitionOfFunc = (beforeEqualToken) => {
 }
 
 /**
+ * @param {Array<String>} afterEqualToken
+ */
+const replaceLambda = (afterEqualToken) => {
+    const lambdaList = new Array();
+    let rbracketIndexList = new Array();
+    for (let i = 0; i < afterEqualToken.length; ++i) {
+        const token = afterEqualToken[i];
+        if (token == '(') {
+            rbracketIndexList.push(i);
+        }
+        else if (token == ')') {
+            const from = rbracketIndexList[rbracketIndexList.length - 1];
+            const to = i;
+            const lambdaName = '$lambda' + lambdaList.length;
+            const before = afterEqualToken.slice(0, from);
+            const after = afterEqualToken.slice(to + 1, afterEqualToken.length);
+            const middle = afterEqualToken.slice(from + 1, to)
+            lambdaList.push(
+                {
+                    name: lambdaName,
+                    token: middle
+                }
+            );
+            afterEqualToken = before.concat([lambdaName]).concat(after);
+            rbracketIndexList.pop();
+            i -= middle.length + 2;
+        }
+    }
+    return {
+        newAfterEqual: afterEqualToken,
+        lambdaList: lambdaList
+    };
+}
+
+/**
+ * @param {Array<String>} afterEqualToken
+ * @returns {String}
+ */
+const processOfFunc = (afterEqualToken) => {
+    const {newAfterEqual, lambdaList} = replaceLambda(afterEqualToken);
+    console.log(lambdaList);
+    console.log(newAfterEqual);
+}
+
+/**
  * @param {TokenLv2} token\
  * @returns {String}
  */
 const generateScript = (token) => {
     const {beforeEqual, afterEqual} = sliceAtEqual(token);
     const def = definitionOfFunc(beforeEqual);
-    const process = '{};';
+    const process = processOfFunc(afterEqual);
     return def + process;
 };
