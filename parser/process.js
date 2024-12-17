97d1129c1593058962a0d6054ae0b25e2c6e2277
nagnagi <nag.nagipon@gmail.com>
2024-12-17 12:58:51 +0900

    process これだけ？？？

---
+++ parser/process.js
@@ -0,0 +1,11 @@
+const add = x0 => x1 => x0 + x1;
+
+const sub = x0 => x1 => x0 - x1;
+
+const mul = x0 => x1 => x0 * x1;
+
+const div = x0 => x1 => x0 / x1;
+
+const mod = x0 => x1 => x0 % x1;
+
+const ifxab = x => a => b => x ? a : b;
